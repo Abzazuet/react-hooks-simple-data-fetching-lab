03326306b15e7eb72fd58a318b92b770330da988
Abel Zazueta <abel.zazueta@gmail.com>
2022-05-31 12:19:26 -0700

    Assignment complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
 // create your App component here
+import React, { useState, useEffect } from "react"
+
+function App() {
+    const [image, setImage] = useState({});
+    const [isLoaded, setLoad] = useState(false);
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+            .then(response => response.json())
+            .then(data => {                
+                setImage(data.message)
+                setLoad(true);
+                
+            });
+    }, []);
+
+    if (!isLoaded) {
+        return <p>Loading...</p>
+    }
+    return <img alt='A Random Dog' src={image}></img>
+
+}
+export default App;
